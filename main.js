bc27127cde12e75c4f219d178542f4e4e62985a3
Anastasia I Bunawan <anastasiaibunawan@Anastasias-MacBook-Air.local>
2022-07-25 15:42:18 +0700

    Added Breadcrumbs

--- main.js
+++ main.js
@@ -110,5 +110,15 @@ function modifyAttributes(id, type){
             profileTab.setAttribute("data-bs-toggle",type);
             contactTab.setAttribute("data-bs-toggle",type);
             break;
+        
+        //Breadcrumbs
+        case "breadcrumb":
+            if(type == "svg"){
+                //document.getElementById('breadcrumb').setAttribute("style","--bs-breadcrumb-divider: url(\x22data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-right-short' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z'/></svg>\x22);");
+                document.getElementById('breadcrumb').setAttribute("style","--bs-breadcrumb-divider: url(\x22data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='black' viewBox='0 0 20 20'><path d='M12.522,10.4l-3.559,3.562c-0.172,0.173-0.451,0.176-0.625,0c-0.173-0.173-0.173-0.451,0-0.624l3.248-3.25L8.161,6.662c-0.173-0.173-0.173-0.452,0-0.624c0.172-0.175,0.451-0.175,0.624,0l3.738,3.736C12.695,9.947,12.695,10.228,12.522,10.4 M18.406,10c0,4.644-3.764,8.406-8.406,8.406c-4.644,0-8.406-3.763-8.406-8.406S5.356,1.594,10,1.594C14.643,1.594,18.406,5.356,18.406,10M17.521,10c0-4.148-3.374-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.147,17.521,10'/></svg>\x22);");
+            } else {
+                document.getElementById('breadcrumb').setAttribute("style","--bs-breadcrumb-divider: '"+type+"';");
+            }
+            break;
     }
 }
